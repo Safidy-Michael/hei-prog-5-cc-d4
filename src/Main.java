71eaf6dbd9f80771f74cc1ab8eeda6a6b8c2430b
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-06-10 09:26:18 +0300

    feat: Displaying an empty 10x10 grid

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        final int SIZE = 10;
+        for (int y = 0; y < SIZE; y++) {
+            for (int x = 0; x < SIZE; x++) {
+                System.out.print(". ");
+            }
+            System.out.println();
+        }
+
+    }
+}
