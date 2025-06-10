e32f0aaf2199e09e719b13c2ac7315880f19a077
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-06-10 09:43:12 +0300

    feat: add basic Snake class with body and direction

--- src/model/Snake.java
+++ src/model/Snake.java
@@ -1,8 +1,22 @@
 package model;
 
-import java.util.List;
+import java.util.LinkedList;
 
 public class Snake {
-    List<Point> body;
+    private final LinkedList<Point> body = new LinkedList<>();
+    private  Direction direction;
 
+    public Snake(Point start, Direction dir){
+        body.add(start);
+        this.direction = dir;
+    }
+
+
+    public void setDirection(Direction d) {
+        this.direction = d;
+    }
+
+    public boolean isOnBody(Point p) {
+        return body.contains(p);
+    }
 }
