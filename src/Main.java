7479c45f45690bab3ad4d40a8ee2743be2f7cad4
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-06-10 09:56:42 +0300

    feat: display grid with snake head at initial position

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,22 @@
+import model.Direction;
+import model.Point;
+import model.Snake;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         final int SIZE = 10;
+        Point start = new Point(5, 5);
+        Snake snake = new Snake(start, Direction.RIGHT);
         for (int y = 0; y < SIZE; y++) {
             for (int x = 0; x < SIZE; x++) {
-                System.out.print(". ");
+                Point current = new Point(x, y);
+                if (current.equals(snake.getHead())) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(". ");
+                }
             }
             System.out.println();
         }
