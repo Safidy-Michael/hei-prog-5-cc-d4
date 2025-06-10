393da59c4c0445709af019ac9832e5921d664b62
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2025-06-10 09:53:38 +0300

    chore: add collision detection with snake body

--- src/model/Snake.java
+++ src/model/Snake.java
@@ -4,13 +4,20 @@ import java.util.LinkedList;
 
 public class Snake {
     private final LinkedList<Point> body = new LinkedList<>();
-    private  Direction direction;
+    private Direction direction;
 
-    public Snake(Point start, Direction dir){
+    public Snake(Point start, Direction dir) {
         body.add(start);
         this.direction = dir;
     }
 
+    public Point getHead() {
+        return body.getFirst();
+    }
+
+    public Direction getDirection() {
+        return direction;
+    }
 
     public void setDirection(Direction d) {
         this.direction = d;
@@ -18,5 +25,10 @@ public class Snake {
 
     public boolean isOnBody(Point p) {
         return body.contains(p);
+    }
+
+    public boolean isCollidingWithItself() {
+        Point head = getHead();
+        return body.stream().skip(1).anyMatch(p -> p.equals(head));
     }
 }
